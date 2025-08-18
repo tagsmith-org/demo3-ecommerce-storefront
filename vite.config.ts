5dd597e7c72566e3c244c873d5e721e87952afec
Serg Kostyuk <serg.kostyuk@gmail.com>
2025-08-18 15:33:04 -0400

    aaaaaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/demo3-ecommerce-storefront/' : '/',
   server: {
     port: 3000,
     open: true
